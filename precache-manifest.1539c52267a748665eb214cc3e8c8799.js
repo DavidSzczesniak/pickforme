c6176f247dd3f814f8a436bc6e8909decdec4d11
DavidSzczesniak <david.szczesniak86@gmail.com>
2020-08-23 23:10:39 +0100

    Updates

---
+++ precache-manifest.1539c52267a748665eb214cc3e8c8799.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fa7c64fd662f9142501a0ea3617ff7b",
+    "url": "/davidszczesniak.github.io/pickforme/index.html"
+  },
+  {
+    "revision": "9a161e6231e60a7ed041",
+    "url": "/davidszczesniak.github.io/pickforme/static/css/main.76c0bdd6.chunk.css"
+  },
+  {
+    "revision": "55dc1dfcd3e857166949",
+    "url": "/davidszczesniak.github.io/pickforme/static/js/2.456dc2e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/davidszczesniak.github.io/pickforme/static/js/2.456dc2e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a161e6231e60a7ed041",
+    "url": "/davidszczesniak.github.io/pickforme/static/js/main.87ac527a.chunk.js"
+  },
+  {
+    "revision": "68fc29062a6d772879d6",
+    "url": "/davidszczesniak.github.io/pickforme/static/js/runtime-main.645ac7d3.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/davidszczesniak.github.io/pickforme/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "ace9c6c799c8cc47a371dddd8833a71b",
+    "url": "/davidszczesniak.github.io/pickforme/static/media/pfm-logo.ace9c6c7.svg"
+  }
+]);
