96a44cf64905b83c2e4d3c803fd484845253c80f
DavidSzczesniak <david.szczesniak86@gmail.com>
2020-08-23 23:07:03 +0100

    Improved page routing
    Edited Main.js to add a redirect for "/" because accessing the page before was blank. Also added a proper path to "home".

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import Home from '../pages/Home';
-import Create from '../pages/Create';
-import Contact from '../pages/Contact';
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Home from "../pages/Home";
+import Create from "../pages/Create";
+import Contact from "../pages/Contact";
 
 const Main = () => {
-    return (
-        <Switch>
-            <Route exact path="/" component={Home} replace></Route>
-            <Route path="/create" exact component={Create} replace></Route>
-            <Route path="/contact" component={Contact} replace></Route>
-        </Switch>
-    );
+  return (
+    <Switch>
+      <Route
+        exact
+        path="/"
+        render={() => {
+          return <Redirect to="/home" />;
+        }}
+      />
+      <Route exact path="/home" component={Home} replace></Route>
+      <Route path="/create" exact component={Create} replace></Route>
+      <Route path="/contact" component={Contact} replace></Route>
+    </Switch>
+  );
 };
 
 export default Main;
