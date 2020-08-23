9302ecbbd5d5f158f0bd74c8a9a461232a319af4
DavidSzczesniak <david.szczesniak86@gmail.com>
2020-08-23 23:24:41 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/davidszczesniak.github.io/pickforme/precache-manifest.1539c52267a748665eb214cc3e8c8799.js"
+  "/pickforme/home/precache-manifest.2dce8b40a4e77562b07310bff2eacd91.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/davidszczesniak.github.io/pickforme/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pickforme/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
