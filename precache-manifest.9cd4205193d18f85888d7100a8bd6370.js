11c7b5da8e8683e5ed22fae7a65f73e5ac92d08a
DavidSzczesniak <david.szczesniak86@gmail.com>
2020-09-19 18:45:41 +0100

    Updates

---
+++ precache-manifest.9cd4205193d18f85888d7100a8bd6370.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1fd2bea7704f1c93d92c39354fccbc",
+    "url": "/pickforme/index.html"
+  },
+  {
+    "revision": "718fbfce18b24725d017",
+    "url": "/pickforme/static/css/main.1d5bd3d3.chunk.css"
+  },
+  {
+    "revision": "55dc1dfcd3e857166949",
+    "url": "/pickforme/static/js/2.456dc2e0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pickforme/static/js/2.456dc2e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "718fbfce18b24725d017",
+    "url": "/pickforme/static/js/main.9b17b541.chunk.js"
+  },
+  {
+    "revision": "4e7bfee5bf2b1982fb10",
+    "url": "/pickforme/static/js/runtime-main.6133bcac.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/pickforme/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "ace9c6c799c8cc47a371dddd8833a71b",
+    "url": "/pickforme/static/media/pfm-logo.ace9c6c7.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1cebde246eb9a89dc0d88573fac5aae4.js"
+  "/pickforme/precache-manifest.9cd4205193d18f85888d7100a8bd6370.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pickforme/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
